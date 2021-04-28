297921b20b848e90061fdee5e94c359607145a58
Frad LEE <fradser@gmail.com>
2021-04-28 22:21:36 +0800

    update eslint config
    - Update eslint-plugin-simple-import-sort
    - Fix sort-keys-fix errors, conflict with sort-keys and delete sort-keys
    rules

--- eslint.js
+++ eslint.js
@@ -8,29 +8,35 @@ module.exports = {
     'prettier/react',
     'prettier/@typescript-eslint'
   ],
-  plugins: [
-    '@typescript-eslint',
-    'simple-import-sort',
-    'sort-keys-fix',
-    'react-hooks'
-  ],
   // See https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md#configuration
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: 'module',
     ecmaFeatures: {
       jsx: true
-    }
+    },
+    sourceType: 'module'
   },
-  settings: {
-    react: {
-      // See https://github.com/preactjs/eslint-config-preact/blob/3ac23ffe761236aae1e5cc5b44614d06d71e3476/index.js#L51-L53
-      pragma: 'h',
-      version: '16.0'
-    }
-  },
+  plugins: [
+    '@typescript-eslint',
+    'react-hooks',
+    'simple-import-sort',
+    'sort-keys-fix'
+  ],
   rules: {
+    // Disable some rules from `@typescript-eslint`
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/member-delimiter-style': ['off'],
+    '@typescript-eslint/no-empty-function': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-use-before-define': ['off'],
+    '@typescript-eslint/no-var-requires': ['off'],
+    // See https://github.com/lydell/eslint-plugin-simple-import-sort/blob/master/README.md#usage
+    'import/order': ['off'],
+    // See https://github.com/eslint/eslint/blob/master/docs/rules/no-console.md
     'no-console': ['error'],
+    // See https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
+    'no-unused-vars': ['off'],
     // See https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-sort-props.md
     'react/jsx-sort-props': [
       'error',
@@ -42,30 +48,25 @@ module.exports = {
     // See https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md
     'react/no-unknown-property': ['off'],
     // See https://github.com/facebook/react/blob/master/packages/eslint-plugin-react-hooks/README.md#installation
-    'react-hooks/rules-of-hooks': ['error'],
     'react-hooks/exhaustive-deps': ['error'],
-    // See https://github.com/lydell/eslint-plugin-simple-import-sort/blob/master/README.md#usage
-    'sort-imports': ['off'],
-    'import/order': ['off'],
-    'simple-import-sort/sort': ['error'],
+    'react-hooks/rules-of-hooks': ['error'],
+    // See https://github.com/lydell/eslint-plugin-simple-import-sort#usage
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
     // See https://github.com/leo-buneev/eslint-plugin-sort-keys-fix#usage
-    'sort-keys': [
+    'sort-keys-fix/sort-keys-fix': [
       'error',
       'asc',
       {
         natural: true
       }
-    ],
-    'sort-keys-fix/sort-keys-fix': ['error'],
-    // See https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
-    'no-unused-vars': ['off'],
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    // Disable some rules from `@typescript-eslint`
-    '@typescript-eslint/explicit-function-return-type': ['off'],
-    '@typescript-eslint/member-delimiter-style': ['off'],
-    '@typescript-eslint/no-empty-function': ['off'],
-    '@typescript-eslint/no-explicit-any': ['off'],
-    '@typescript-eslint/no-use-before-define': ['off'],
-    '@typescript-eslint/no-var-requires': ['off']
+    ]
+  },
+  settings: {
+    react: {
+      // See https://github.com/preactjs/eslint-config-preact/blob/3ac23ffe761236aae1e5cc5b44614d06d71e3476/index.js#L51-L53
+      pragma: 'h',
+      version: '16.0'
+    }
   }
 }
