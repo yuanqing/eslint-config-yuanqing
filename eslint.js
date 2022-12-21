915b91c141781458738e89647a1191cc0d52b87e
Yuan Qing Lim <desk@yuanqing.sg>
2022-12-21 14:13:11 +0800

    Bump dependencies

--- eslint.js
+++ eslint.js
@@ -1,20 +1,15 @@
 module.exports = {
   extends: [
-    // https://github.com/yannickcr/eslint-plugin-react/blob/master/README.md#configuration
+    // https://eslint.org/docs/latest/user-guide/configuring/configuration-files#using-eslintrecommended
+    'eslint:recommended',
+    // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/README.md#configuration-legacy-eslintrc
     'plugin:react/recommended',
-    // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md#configuration
+    // https://typescript-eslint.io/getting-started/#step-2-configuration
     'plugin:@typescript-eslint/recommended',
-    // https://github.com/prettier/eslint-config-prettier/blob/master/README.md#special-rules
-    'plugin:prettier/recommended',
-    'prettier'
+    // https://github.com/prettier/eslint-plugin-prettier/blob/master/README.md#recommended-configuration
+    'plugin:prettier/recommended'
   ],
-  plugins: [
-    '@typescript-eslint',
-    'react-hooks',
-    'simple-import-sort',
-    'sort-keys-fix'
-  ],
-  // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/README.md#configuration
+  // https://typescript-eslint.io/getting-started/#step-2-configuration
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
@@ -22,9 +17,19 @@ module.exports = {
       jsx: true
     }
   },
+  plugins: [
+    // https://typescript-eslint.io/getting-started/#step-2-configuration
+    '@typescript-eslint',
+    // https://github.com/facebook/react/blob/main/packages/eslint-plugin-react-hooks/README.md#custom-configuration
+    'react-hooks',
+    // https://github.com/lydell/eslint-plugin-simple-import-sort/blob/main/README.md#usage
+    'simple-import-sort',
+    // https://github.com/leo-buneev/eslint-plugin-sort-keys-fix/blob/master/README.md#usage
+    'sort-keys-fix'
+  ],
   settings: {
+    // https://github.com/preactjs/eslint-config-preact/blob/9dc5fc3682bb2da91209e05af4dcb150b24bec25/index.js#L51-L55
     react: {
-      // https://github.com/preactjs/eslint-config-preact/blob/9dc5fc3682bb2da91209e05af4dcb150b24bec25/index.js#L51-L55
       pragma: 'h',
       version: '16.0'
     }
